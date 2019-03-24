aa27b6c11f1622079a23b248ace4271f371915d3
walkeknow <kewalshah077@gmail.com>
2019-03-25 01:56:05 +0530

    Deploy Django project on Heroku using Python Virtual Environment

---
+++ lib/python3.6/_collections_abc.py
@@ -0,0 +1,1 @@
+/home/walkeknow/anaconda3/lib/python3.6/_collections_abc.py
--- medicine/medicine/settings.py
+++ medicine/medicine/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -115,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
